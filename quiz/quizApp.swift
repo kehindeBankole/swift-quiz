cbcb8e80417793519dcd1bd27222a77e769e58ad
kehindeBankole <bankolek1@gmail.com>
2024-02-24 21:07:09 +0100

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by kehinde on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
